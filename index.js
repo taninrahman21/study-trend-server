2ead43063fb24c164e20d4081acdfdea193b491a
Tanin Rahman <taninr87@gmail.com>
2022-10-26 00:35:20 +0600

    add description and photo url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = 5000;
 
-app.use(cors())
+app.use(cors());
 
 const courses = require('./data/courses.json');
 
@@ -13,6 +13,11 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
   res.send(courses);
+})
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const course = courses.find(course => course.id == id);
+  res.send(course);
 })
 
 app.listen(port, () => {
